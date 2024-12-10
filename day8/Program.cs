7ff4791d1263860693a523438d4d665ce74b715a
Nabeel Sulieman <11143071+nabsul@users.noreply.github.com>
2024-12-09 19:39:19 -0800

    day 8 and 9

---
+++ day8/Program.cs
@@ -0,0 +1,119 @@
+﻿var Rows = 0;
+var Cols = 0;
+var antennae = new Dictionary<char, List<Position>>();
+ReadAntennae();
+
+Console.WriteLine($"Rows: {Rows}, Cols: {Cols}");
+Console.WriteLine($"Antennae: {antennae.Sum(a => a.Value.Count)}");
+
+//GenerateMap();
+
+var allAntinodes = antennae.Values
+    .SelectMany(p => GetAllPairs(p.Count).SelectMany(pair => GetAntinodes(p[pair.Item1], p[pair.Item2])))
+    .Where(IsInBounds)
+    .Distinct();
+
+Console.WriteLine($"Part1: {allAntinodes.Count()}");
+
+var allAntinodesPart2 = antennae.Values
+    .SelectMany(p => GetAllPairs(p.Count).SelectMany(pair => GetAntinodesPart2(p[pair.Item1], p[pair.Item2])))
+    .Distinct();
+
+Console.WriteLine($"Part2: {allAntinodesPart2.Count()}");
+
+void ReadAntennae()
+{
+    foreach (var line in File.ReadLines("data.txt"))
+    {
+        Cols = 0;
+        foreach (var c in line)
+        {
+            if (c != '.')
+            {
+                if (!antennae.TryGetValue(c, out var a))
+                {
+                    antennae[c] = a = [];
+                }
+                a.Add(new Position(Rows, Cols));
+            }
+
+            Cols++;
+        }
+        Rows++;
+    }
+}
+
+IEnumerable<(int, int)> GetAllPairs(int len)
+{
+    for (var i = 0; i < len; i++)
+    {
+        for (var j = i + 1; j < len; j++)
+        {
+            yield return (i, j);
+        }
+    }
+}
+
+IEnumerable<Position> GetAntinodes(Position a, Position b)
+{
+    var dr = a.R - b.R;
+    var dc = a.C - b.C;
+    yield return new Position(a.R + dr, a.C + dc);
+    yield return new Position(b.R - dr, b.C - dc);
+}
+
+IEnumerable<Position> GetAntinodesPart2(Position a, Position b)
+{
+    var dr = a.R - b.R;
+    var dc = a.C - b.C;
+
+    var p = new Position(a.R, a.C);
+    while (IsInBounds(p))
+    {
+        yield return p;
+        p = new Position(p.R + dr, p.C + dc);
+    }
+
+    p = new Position(b.R, b.C);
+    while (IsInBounds(p))
+    {
+        yield return p;
+        p = new Position(p.R - dr, p.C - dc);
+    }
+}
+
+bool IsInBounds(Position p) => p.R >= 0 && p.R < Rows && p.C >= 0 && p.C < Cols;
+
+void GenerateMap()
+{
+    var map = new char[Rows, Cols];
+    for (var r = 0; r < Rows; r++)
+    {
+        for (var c = 0; c < Cols; c++)
+        {
+            map[r, c] = '.';
+        }
+    }
+
+    foreach (var (c, positions) in antennae)
+    {
+        foreach (var p in positions)
+        {
+            map[p.R, p.C] = c;
+        }
+    }
+
+    Console.WriteLine("Map:");
+    for (var r = 0; r < Rows; r++)
+    {
+        for (var c = 0; c < Cols; c++)
+        {
+            Console.Write(map[r, c]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+record Position(int R, int C);
+
---
+++ day9/Program.cs
@@ -0,0 +1,155 @@
+﻿
+using System.Text.Json;
+using System.Xml;
+
+var input = ReadFile();
+if (input.Length % 2 == 0) input = input.Take(input.Length - 1).ToArray();
+
+Console.WriteLine($"Part1 Checksum: {ChecksumPart1(input)}");
+
+input = ReadFile();
+if (input.Length % 2 == 0) input = input.Take(input.Length - 1).ToArray();
+Console.WriteLine($"Part2 Checksum: {ChecksumPart2(input)}");
+
+int[] ReadFile()
+{
+    return File.ReadAllText("data.txt")
+        .ToCharArray()
+        .Select(x => int.Parse(x.ToString()))
+        .ToArray();
+}
+
+long ChecksumPart1(int[] input)
+{
+    return Compact(input)
+    .Select((v, i) => (long)v * i)
+    .Sum();
+}
+
+long ChecksumPart2(int[] input)
+{
+    long sum = 0;
+
+    var data = new LinkedList<Item>(ReadItems(input));
+
+    var curr = data.Last;
+    while (curr != null)
+    {
+        var search = data.First;
+        while (search != null && search != curr && curr.Value.Size > search.Next!.Value.Position - search.Value.Position - search.Value.Size)
+        {
+            search = search.Next;
+        }
+
+        if (search != null && search != curr)
+        {
+            var next = curr.Previous;
+            data.Remove(curr);
+            data.AddAfter(search, curr);
+            curr.Value.Position = search.Value.Position + search.Value.Size;
+            curr = next;
+            continue;
+        }
+
+        curr = curr.Previous;
+    }
+
+    foreach (var node in data)
+    {
+        for (int i = 0; i < node.Size; i++)
+        {
+            sum += node.Id * (node.Position + i);
+        }
+    }
+
+    return sum;
+}
+
+void CompactPart2(LinkedList<Item> items, LinkedListNode<Item> item)
+{
+    if (item.Next == null) return;
+
+    var freeSpace = item.Next.Value.Position - item.Value.Position - item.Value.Size;
+
+    var end = items.Last;
+    while (end != null && end != item.Next)
+    {
+        if (end.Value.Size > freeSpace)
+        {
+            end = end.Previous;
+            continue;
+        }
+
+        items.Remove(end);
+        end.Value.Position = item.Value.Position + item.Value.Size;
+        items.AddAfter(item, end);
+        break;
+    }
+}
+
+IEnumerable<Item> ReadItems(int[] input)
+{
+    int idx = 0;
+    int pos = 0;
+    
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            yield return new Item { Id = idx++, Position = pos, Size = input[i] };
+        }
+        pos += input[i];
+    }
+}
+
+IEnumerable<int> Compact(int[] input)
+{
+    var start = Expand(input).GetEnumerator();
+    var end = ExpandReversed(input).GetEnumerator();
+    var remaining = input.Sum();
+
+    if (!start.MoveNext() || !end.MoveNext()) throw new InvalidOperationException("Invalid input");
+
+    while (remaining > 0)
+    {
+        if (start.Current >= 0)
+        {
+            yield return start.Current;
+            remaining--;
+            start.MoveNext();
+            continue;
+        }
+
+        if (end.Current < 0)
+        {
+            end.MoveNext();
+            remaining--;
+            continue;
+        }
+
+        yield return end.Current;
+        end.MoveNext();
+        start.MoveNext();
+        remaining -= 2;
+    }
+}
+
+IEnumerable<int> Expand(int[] input)
+{
+    return input.SelectMany((v, i) => Enumerable.Repeat(i % 2 == 0 ? i / 2 : -1, v));
+}
+
+IEnumerable<int> ExpandReversed(int[] input)
+{
+    return input.Select((v, i) => new { v, i })
+        .Reverse()
+        .SelectMany(x => Enumerable.Repeat(x.i % 2 == 0 ? x.i / 2 : -1, x.v));
+}
+
+class Item
+{
+    public int Id { get; set; }
+    public int Position { get; set; }
+    public int Size {get;set;}
+    public bool Moved {get;set;}
+}
