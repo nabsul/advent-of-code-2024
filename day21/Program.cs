337b5124a4096323f8e5276eab9d2d7e1f7ca3ba
Nabeel Sulieman <11143071+nabsul@users.noreply.github.com>
2024-12-22 07:59:47 -0800

    Partially solved 21, 22 complete.

---
+++ day21/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+
+var debug = true;
+var keyPad1 = ReadKeyPad("keypad1.txt");
+var keyPad2 = ReadKeyPad("keypad2.txt");
+Dictionary<char, (int, int)> dirs = new ()
+{
+    ['^'] = (-1, 0),
+    ['v'] = (1, 0),
+    ['<'] = (0, -1),
+    ['>'] = (0, 1),
+};
+
+//Solve("test1.txt");
+//Console.WriteLine();
+Reverse("<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A");
+Reverse("v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A");
+//Solve("input.txt");
+
+void Solve(string file)
+{
+    Console.WriteLine($"Solving {file}");
+    var totalScore = 0;
+    foreach (var line in File.ReadAllLines(file))
+    {
+        var sequence = string.Join("", GenerateKeys(line));
+        var val = int.Parse(line[..^1]);
+        var score = sequence.Length * val;
+        totalScore += score;
+        Console.WriteLine($"{line} -> {sequence} -> {sequence.Length}x{val}={score}");
+    }
+    Console.WriteLine($"Total score: {totalScore}");
+}
+
+IEnumerable<char> GenerateKeys(string line)
+{
+    if (debug) Console.WriteLine($"Generating keys for {line}");
+    line = GenerateSequence(line, keyPad1); // to robot 1
+    if (debug) Console.WriteLine($"Robot 1: {line}");
+    line = GenerateSequence(line, keyPad2); // to robot 2
+    if (debug) Console.WriteLine($"Robot 2: {line}");
+    line = GenerateSequence(line, keyPad2); // to human
+    if (debug) Console.WriteLine($"Human: {line}");
+    return line;
+}
+
+string GenerateSequence(string line, Dictionary<char, (int, int)> keyPad)
+{
+    var res = Enumerable.Empty<char>();
+    var pos = keyPad['A'];
+    foreach (var c in line)
+    {
+        var newPos = keyPad[c];
+        res = res.Concat(PressButton(pos, newPos, c));
+        pos = newPos;
+    }
+
+    return string.Join("", res);
+}
+
+IEnumerable<char> PressButton((int, int) pos1, (int, int) pos2, char c)
+{
+    var res = Enumerable.Empty<char>();
+    var (i, j) = Diff(pos2, pos1);
+    if (j > 0) res = res.Concat(Enumerable.Repeat('>', j));
+    if (i > 0) res = res.Concat(Enumerable.Repeat('v', i));
+    if (i < 0) res = res.Concat(Enumerable.Repeat('^', -i));
+    if (j < 0) res = res.Concat(Enumerable.Repeat('<', -j));
+    res = res.Append('A');
+
+    return res;
+}
+
+Dictionary<char, (int, int)> ReadKeyPad(string file)
+{
+    var res = new Dictionary<char, (int, int)>();
+    var lines = File.ReadAllLines(file);
+    for (int i = 0; i < lines.Length; i++)
+    {
+        for (int j = 0; j < lines[i].Length; j++)
+        {
+            if (lines[i][j] != ' ')
+            {
+                res.Add(lines[i][j], (i, j));
+            }
+        }
+    }
+    return res;
+}
+
+(int, int) Diff((int, int) a, (int, int) b) => (a.Item1 - b.Item1, a.Item2 - b.Item2);
+(int, int) Add((int, int) a, (int, int) b) => (a.Item1 + b.Item1, a.Item2 + b.Item2);
+
+
+void Reverse(string line)
+{
+    Console.WriteLine($"Reversing {line}");
+    line = Expand(line, keyPad2);
+    Console.WriteLine($"Robot 2: {line}");
+    line = Expand(line, keyPad2);
+    Console.WriteLine($"Robot 1: {line}");
+    line = Expand(line, keyPad1);
+    Console.WriteLine($"Result: {line}");
+}
+
+string Expand(string line, Dictionary<char, (int, int)> keyPad)
+{
+    var res = new StringBuilder();
+    var pos = keyPad['A'];
+    foreach (var c in line)
+    {
+        if (c != 'A')
+        {
+            var d = dirs[c];
+            pos = Add(pos, d);
+            continue;
+        }
+
+        var val = keyPad.First(x => x.Value == pos).Key;
+        res.Append(val);
+    }
+
+    return res.ToString();
+}
---
+++ day22/Program.cs
@@ -0,0 +1,110 @@
+﻿
+var debug = false;
+
+Solve("test.txt", 2000);
+Solve("input.txt", 2000);
+GetBestPrice("test.txt", 2000);
+GetBestPrice("test2.txt", 2000);
+GetBestPrice("input.txt", 2000);
+
+void Solve(string file, int iterations)
+{
+    var nums = Parse(file).ToArray();
+    var res = nums.Select(n => GetNthSecretNumber(n, iterations)).ToArray();
+
+    Console.WriteLine($"Results {file}:");
+    if (debug)
+    {
+        foreach (var (k, v) in nums.Zip(res))
+        {
+            Console.WriteLine($"{k}: {v}");
+        }
+    }
+    Console.WriteLine($"Sum: {res.Sum()}");
+    Console.WriteLine();
+}
+
+void GetBestPrice(string file, int iterations)
+{
+    var res = new Dictionary<(int, int, int, int), int>();
+    var nums = Parse(file).ToArray();
+    foreach (var n in nums)
+    {
+        foreach (var (k, v) in Prices(n, iterations))
+        {
+            if (!res.TryGetValue(k, out var prev))
+            {
+                prev = 0;
+            }
+            res[k] = v + prev;
+        }
+    }
+
+    var (key, val) = res.MaxBy(p => p.Value);
+    Console.WriteLine($"Best price {file}: {key} -> {val}");
+}
+
+Dictionary<(int, int, int, int), int> Prices(long num, int iterations)
+{
+    var res = new Dictionary<(int, int, int, int), int>();
+
+    foreach(var (key, val) in GetPrices(num).Take(iterations).Skip(3))
+    {
+        if (!res.TryGetValue(key, out var prev))
+        {
+            res.Add(key, val);
+        }
+    }
+
+    return res;
+}
+
+IEnumerable<((int, int, int, int), int)> GetPrices(long num)
+{
+    var key = (0, 0, 0, 0);
+    var prev = (int)(num % 10);
+    foreach (var s in GetSecretNumbers(num).Select(n => (int)(n % 10)))
+    {
+        var diff = s - prev;
+        var (a, b, c, d) = key;
+        key = (b, c, d, diff);
+        yield return (key, s);
+        prev = s;
+    }
+}
+
+long GetNthSecretNumber(long num, int n)
+{
+    return GetSecretNumbers(num).Skip(n-1).First();
+}
+
+IEnumerable<long> GetSecretNumbers(long num)
+{
+    while (true)
+    {
+        num = NextSecretNumber(num);
+        yield return num;
+    }
+}
+
+long NextSecretNumber(long num)
+{
+    var step1 = Prune(Mix(num, num * 64));
+    var step2 = Prune(Mix(step1, step1 / 32));
+    return Prune(Mix(step2, step2 * 2048));
+}
+
+long Mix(long a, long b) => a ^ b;
+long Prune(long a) => a % 16777216;
+
+IEnumerable<long> Parse(string file)
+{
+    using (var reader = new StreamReader(file))
+    {
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            yield return long.Parse(line);
+        }
+    }
+}
