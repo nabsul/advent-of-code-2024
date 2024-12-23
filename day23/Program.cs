a42a8b2ea1ae78eb7ec638007aa744798b6a4c04
Nabeel Sulieman <11143071+nabsul@users.noreply.github.com>
2024-12-22 21:28:44 -0800

    Start on day23

---
+++ day23/Program.cs
@@ -0,0 +1,22 @@
+﻿var file = "test.txt";
+
+var lines = File.ReadAllLines(file)
+    .Select(l => l.Split('-'));
+
+Dictionary<string, HashSet<string>> dict = [];
+
+foreach (var line in lines)
+{
+    void Update(string key, string value)
+    {
+        if (!dict.TryGetValue(key, out var hash))
+        {
+            dict[key] = hash = [];
+        }
+
+        dict[key].Add(value);
+    }
+    Update(line[0], line[1]);
+    Update(line[1], line[0]);
+}
+
