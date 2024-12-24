f1295565f6950113b924a0b8af0f5e1400b91b6e
Nabeel Sulieman <11143071+nabsul@users.noreply.github.com>
2024-12-23 22:08:27 -0800

    Solved day 23

--- day23/Program.cs
+++ day23/Program.cs
@@ -1,105 +1,82 @@
 ﻿
 Solve("test.txt", 3);
-//Solve("input.txt", 3);
+Solve("input.txt", 3);
 
 void Solve(string file, int size)
 {
-    var conns = ReadConnections(file);
-    var sets = BuildSets(conns).ToArray();
-    var subSets = BuildSubsets(sets, size)
-        .Where(s => s.Any(v => v[0] == 't'))
-        .ToArray();
-
-    Console.WriteLine("Sets:");
-    foreach (var set in sets)
-    {
-        Console.WriteLine(string.Join(", ", set));
-    }
-
-    Console.WriteLine("\nSubsets:");
-    foreach (var set in subSets)
-    {
-        Console.WriteLine(string.Join(", ", set));
-    }
+    var conns = ReadConnections(file).ToArray();
+    var subSets = CollapseSets(conns).ToArray();
+    var filtered = subSets.Where(s => s.Any(v => v[0] == 't')).ToArray();
 
     Console.WriteLine("\nSets found: {0}", subSets.Length);
-    Console.WriteLine("Subsets found: {0}", subSets.Length);
-}
+    Console.WriteLine("Filtered found: {0}", filtered.Length);
 
-IEnumerable<HashSet<string>> BuildSubsets(IEnumerable<HashSet<string>> sets, int size)
-{
-    foreach (var set in sets.Where(s => s.Count >= size))
+    while (subSets.Length > 1)
     {
-        foreach (var combo in GenerateCombos([..set], size))
-        {
-            yield return combo;
-        }
+        subSets = CollapseSets(subSets).ToArray();
+    }
+
+    if (subSets.Length == 1)
+    {
+        Console.WriteLine("Final set: {0}", string.Join(",", subSets[0]));
+    }
+    else
+    {
+        Console.WriteLine("No final set found");
     }
 }
 
-IEnumerable<HashSet<string>> GenerateCombos(string[] vals, int size)
+IEnumerable<string[]> CollapseSets(IEnumerable<string[]> sets)
 {
-    var ptrs = Enumerable.Range(0, size).ToArray();
-    do
-    {
-        yield return ptrs.Select(i => vals[i]).ToHashSet();
-    } while (Increment(ptrs, vals.Length));
-}
+    var list = sets.Select(s => string.Join("-", s)).OrderBy(s => s).ToList();
+    var lookup = list.ToHashSet();
+    var idx = 0;
 
-bool Increment(int[] ptrs, int max)
-{
-    var i = ptrs.Length - 1;
-    while (i >= 0 && ptrs[i] == max - ptrs.Length + i) i--;
-    if (i < 0) return false;
-    ptrs[i]++;
-    while (++i < ptrs.Length) ptrs[i] = ptrs[i - 1] + 1;
-    return true;
-}
+    //Console.WriteLine("List: {0}", string.Join(", ", list));
 
-IEnumerable<HashSet<string>> BuildSets(Dictionary<string, HashSet<string>> conns)
-{
-    SortedSet<string> nodes = [..conns.Keys.OrderByDescending(k => conns[k].Count)];
-    while (nodes.Count > 0)
+    while (idx < list.Count - 1)
     {
-        var node = nodes.First();
-        var set = BuildSet(node, conns);
-        yield return set;
-        nodes.ExceptWith(set);
-    }
-}
-
-HashSet<string> BuildSet(string seed, Dictionary<string, HashSet<string>> conns)
-{
-    HashSet<string> set = [seed];
-    foreach (var (k, v) in conns.Where(p => p.Key != seed))
-    {
-        if (v.IsSupersetOf(set))
+        var idx2 = idx + 1;
+        while (idx2 < list.Count && IsPossible(list[idx], list[idx2]))
         {
-            set.Add(k);
-        }
-    }
-    return set;
-}
-
-Dictionary<string, HashSet<string>> ReadConnections(string file)
-{
-    var lines = File.ReadAllLines(file)
-        .Select(l => l.Split('-'));
-
-    Dictionary<string, HashSet<string>> dict = [];
-    foreach (var line in lines)
-    {
-        void Update(string key, string value)
-        {
-            if (!dict.TryGetValue(key, out var hash))
+            var combos = GetCombos(list[idx], list[idx2]).ToArray();
+            //Console.WriteLine($"Combos: {string.Join(", ", combos)}");
+            if (combos.All(c => lookup.Contains(c)))
             {
-                dict[key] = hash = [];
+                yield return MakeSuperSet(list[idx], list[idx2]);
             }
 
-            hash.Add(value);
+            idx2++;
         }
-        Update(line[0], line[1]);
-        Update(line[1], line[0]);
+
+        idx++;
     }
-    return dict;
+}
+
+bool IsPossible(string a, string b)
+{
+    return a[..^2] == b[..^2];
+}
+
+IEnumerable<string> GetCombos(string a, string b)
+{
+    var parts = MakeSuperSet(a, b);
+    for (int i = parts.Length - 1; i >= 0; i--)
+    {
+        yield return string.Join("-", parts.Where((_, j) => j != i));
+    }
+}
+
+string[] MakeSuperSet(string a, string b)
+{
+    var parts1 = a.Split('-');
+    var parts2 = b.Split('-');
+    var parts = parts1.Append(parts2.Last()).ToArray();
+    return parts;
+}
+
+IEnumerable<string[]> ReadConnections(string file)
+{
+    return File.ReadAllLines(file)
+        .Select(l => l.Split('-').OrderBy(v => v).ToArray());
 }
