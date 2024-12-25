473b9a8326d2584a9a6a6504827d00554915de91
Nabeel Sulieman <11143071+nabsul@users.noreply.github.com>
2024-12-25 08:12:09 -0800

    decompile for debugging

--- day17/Program.cs
+++ day17/Program.cs
@@ -1,5 +1,9 @@
 ﻿Run("test.txt");
+Decompile("test.txt");
+Run("test2.txt");
+Decompile("test2.txt");
 Run("input.txt");
+Decompile("input.txt");
 
 void Run(string file)
 {
@@ -10,7 +14,7 @@ void Run(string file)
 
 IEnumerable<long> RunProgram(long[] ops, long a, long b, long c)
 {
-    Console.WriteLine($"Running program with a={a}, b={b}, c={c}");
+    Console.WriteLine($"Running program with a={a} ({Convert.ToString(a, 2)}), b={b} ({Convert.ToString(b, 2)}), c={c} ({Convert.ToString(c, 2)})");
     Console.WriteLine($"Program: {string.Join(",", ops)}");
     var pos = 0;
     var comboVals = new long[] { 0, 1, 2, 3, a, b, c };
@@ -39,6 +43,37 @@ IEnumerable<long> RunProgram(long[] ops, long a, long b, long c)
     }
 }
 
+void Decompile(string file)
+{
+    var (ops, a, b, c) = Parse(file);
+    Console.WriteLine($"Decompiling program {file}");
+    Console.WriteLine($"Initial values: a={a}, b={b}, c={c}");
+    var pos = 0;
+    var combos = new string[] { "0", "1", "2", "3", "a", "b", "c" };
+    while (pos < ops.Length)
+    {
+        var line = pos.ToString("D2");
+        var op = ops[pos];
+        var par = ops[pos + 1];
+        pos += 2;
+
+        Console.Write($"{line}: ");
+        switch (op)
+        {
+            case 0: Console.WriteLine($"a /= 2^{combos[par]}"); break;
+            case 1: Console.WriteLine($"b ^= {par}"); break;
+            case 2: Console.WriteLine($"b = {combos[par]} & 111"); break;
+            case 3: Console.WriteLine($"if (a != 0) goto {par}"); break;
+            case 4: Console.WriteLine($"b ^= c"); break;
+            case 5: Console.WriteLine($"output: {combos[par]} & 111"); break;
+            case 6: Console.WriteLine($"b = a / 2^{combos[par]}"); break;
+            case 7: Console.WriteLine($"c = a / 2^{combos[par]}"); break;
+            default: throw new Exception("Invalid opcode");
+        }
+    }
+    Console.WriteLine();
+}
+
 (long[] ops, long a, long b, long c) Parse(string file)
 {
     var lines = File.ReadAllLines(file);
@@ -48,3 +83,4 @@ IEnumerable<long> RunProgram(long[] ops, long a, long b, long c)
     var code = lines[4].Split(": ")[1].Split(',').Select(long.Parse).ToArray();
     return (code, a, b, c);
 }
+
