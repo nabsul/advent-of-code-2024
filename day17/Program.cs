f5d5bbf04d912f037c945c38a4d04de17cd6125c
Nabeel Sulieman <11143071+nabsul@users.noreply.github.com>
2024-12-24 17:37:41 -0800

    Simplify the code, make the output enumerable so I can abort any time.

--- day17/Program.cs
+++ day17/Program.cs
@@ -1,86 +1,50 @@
 ﻿Run("test.txt");
 Run("input.txt");
-Run("fixed.txt");
 
 void Run(string file)
 {
-    var c = Parse(file);
-
-    while (c.Pos < c.Code.Length)
-    {
-        Exec(c);
-    }
-
-    Console.WriteLine($"Program {file} output: " + string.Join(",", c.Outputs));
+    var (ops, a, b, c) = Parse(file);
+    var output = RunProgram(ops, a, b, c).ToArray();
+    Console.WriteLine($"Program {file} output: {string.Join(",", output)}\n");
 }
 
-void Exec(ComputerState c)
+IEnumerable<long> RunProgram(long[] ops, long a, long b, long c)
 {
-    var op = c.Code[c.Pos];
-    var par = c.Code[c.Pos + 1];
-    c.Pos += 2;
-    Action<long> func = op switch
-    {
-        0 => Adv,
-        1 => Bxl,
-        2 => Bst,
-        3 => Jnz,
-        4 => Bxc,
-        5 => Out,
-        6 => Bdv,
-        7 => Cdv,
-        _ => throw new Exception("Invalid opcode")
-    };
+    Console.WriteLine($"Running program with a={a}, b={b}, c={c}");
+    Console.WriteLine($"Program: {string.Join(",", ops)}");
+    var pos = 0;
+    var comboVals = new long[] { 0, 1, 2, 3, a, b, c };
 
-    func(par);
-
-    // operations
-    void Adv(long op) => c.A /= (long)Math.Pow(2, ComboVal(op));
-    void Bxl(long op) => c.B ^= op;
-    void Bst(long op) => c.B = ComboVal(op) % 8;
-    void Jnz(long op) => c.Pos = c.A != 0 ? (int)op : c.Pos;
-    void Bxc(long _) => c.B ^= c.C;
-    void Out(long op)
+    while (pos < ops.Length)
     {
-        var v = ComboVal(op) % 8;
-        c.Outputs.Add(v);
-    } 
+        var op = ops[pos];
+        var par = ops[pos + 1];
+        comboVals[4] = a;
+        comboVals[5] = b;
+        comboVals[6] = c;
+        pos += 2;
 
-    void Bdv(long op) => c.B = c.A / (long)Math.Pow(2, ComboVal(op));
-    void Cdv(long op) => c.C = c.A / (long)Math.Pow(2, ComboVal(op));
-    
-    long ComboVal(long op)
-    {
-        return op switch
+        switch (op)
         {
-            0 => 0,
-            1 => 1,
-            2 => 2,
-            3 => 3,
-            4 => c.A,
-            5 => c.B,
-            6 => c.C,
-            _ => throw new Exception("Invalid operand")
-        };
+            case 0: a /= (long)Math.Pow(2, comboVals[par]); break;
+            case 1: b ^= par; break;
+            case 2: b = comboVals[par] % 8; break;
+            case 3: pos = a != 0 ? (int)par : pos; break;
+            case 4: b ^= c; break;
+            case 5: yield return comboVals[par] % 8; break;
+            case 6: b = a / (long)Math.Pow(2, comboVals[par]); break;
+            case 7: c = a / (long)Math.Pow(2, comboVals[par]); break;
+            default: throw new Exception("Invalid opcode");
+        }
     }
 }
 
-ComputerState Parse(string file)
+(long[] ops, long a, long b, long c) Parse(string file)
 {
     var lines = File.ReadAllLines(file);
     var a = long.Parse(lines[0].Split(": ")[1]);
     var b = long.Parse(lines[1].Split(": ")[1]);
     var c = long.Parse(lines[2].Split(": ")[1]);
     var code = lines[4].Split(": ")[1].Split(',').Select(long.Parse).ToArray();
-    return new ComputerState(a, b, c, code);
-}
-
-class ComputerState(long a, long b, long c, long[] code)
-{
-    public int Pos { get; set; } = 0;
-    public long A { get; set; } = a;
-    public long B { get; set; } = b;
-    public long C { get; set; } = c;
-    public long[] Code { get; } = code;
-    public List<long> Outputs { get; } = [];
+    return (code, a, b, c);
 }
